0308c254edafe64a28229ccd691a8f758f2ccdbc
chanho lee <leech3878@naver.com>
2023-07-10 19:48:50 +0900

    기본적인 틀 제작

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,143 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '또래또래를 소개합니다!',
-          ),
+      title: 'TeamProject',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        bottomNavigationBar: TabBar(
+          //(1)
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.people), text: "팀"),
+            Tab(
+              icon: Icon(Icons.person),
+              text: "팀원",
+            ),
+          ],
+          indicatorColor: Colors.transparent, // indicator 없애기
+          unselectedLabelColor: Colors.grey, // 선택되지 않은 tab 색
+          labelColor: Colors.black, // 선택된 tab의 색
         ),
+        body: TabBarView(
+          children: <Widget>[
+            Tab(
+              child: Team(),
+            ),
+            Tab(
+              child: TeamMember(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// 팀소개 페이지
+class Team extends StatelessWidget {
+  const Team({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('팀 소개합니다'),
+      ),
+    );
+  }
+}
+
+// 팀원 소개 페이지
+class TeamMember extends StatelessWidget {
+  const TeamMember({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('팀원 소개'),
+      ),
+      body: Text('팀원을 등록해주세요'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MemberAdd()),
+          );
+        },
+        backgroundColor: Color(0xFFFF7E36),
+        elevation: 1,
+        child: Icon(
+          Icons.add_rounded,
+          size: 36,
+        ),
+      ),
+    );
+  }
+}
+
+//팀원 등록 페이지
+class MemberAdd extends StatelessWidget {
+  const MemberAdd({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('이곳은 팀원 등록 페이지입니다'),
+      ),
+    );
+  }
+}
+
+//팀원 상세 설명 페이지
+class MemberDetail extends StatelessWidget {
+  const MemberDetail({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('이곳은 팀원 등록 페이지입니다'),
+      ),
+    );
+  }
+}
+
+//팀원 정보 수정 페이지
+class MemberAdjust extends StatelessWidget {
+  const MemberAdjust({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('이곳은 팀원 정보 수정 페이지입니다'),
       ),
     );
   }
