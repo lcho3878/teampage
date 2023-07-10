950cc88cabd77e9bef8e610285a403537d416ced
t2023-m079 <t2023-m079@t2023-m079ui-MacBookAir.local>
2023-07-10 16:33:10 +0900

    초기파일 생성

---
+++ android/app/src/main/kotlin/com/example/teampage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teampage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            '또래또래를 소개합니다!',
+          ),
+        ),
+      ),
+    );
+  }
+}
