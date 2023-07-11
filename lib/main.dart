70d44df14fe69897c7110617f46416c82fb9cb13
chanho lee <leech3878@naver.com>
2023-07-11 11:39:29 +0900

    사진 및 Ui수정

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,116 @@ class Team extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('팀 소개합니다'),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        title: Text(
+          '또래또래를 소개합니다',
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(60),
+              child: Image.network(
+                'https://rectangular-shape-82a.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F8809b63e-8d46-4946-a5e0-4a71a1d45e5b%2F8team.png?id=575e603a-56c4-4aee-b463-f9e93beb8efd&table=block&spaceId=6b9ebab3-09ea-4406-a18e-30ecb1ca7abb&width=2000&userId=&cache=v2',
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              height: 70,
+              child: Card(
+                child: Center(
+                  child: Text(
+                    '팀 목표 : 프로젝트 끝까지 잘 마무리하기!',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                color: Colors.pink.shade100,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              height: 100,
+              child: Card(
+                child: Center(
+                  child: Text(
+                    '팀 특징 : 비슷한 또래들끼리 모여 또래또래가 되었다.',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                color: Color.fromARGB(255, 129, 229, 170),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              height: 200,
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '우리팀 약속',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        '• Git Merge는 매일 6시에 하기',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '• 식사시간은 점심 PM 1:00, 저녁 PM 6:00',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      Text(
+                        '• 매일 2시간 이상 수강 및 복습 후 팀프로젝트 참여하기',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                color: Colors.blue.shade300,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusDirectional.circular(8),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -137,7 +246,11 @@ class MemberAdjust extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('이곳은 팀원 정보 수정 페이지입니다'),
+        title: Column(
+          children: [
+            Text('이곳은 팀원 정보 수정 페이지입니다'),
+          ],
+        ),
       ),
     );
   }
