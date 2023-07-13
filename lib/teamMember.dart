74c2009635382867fc72449857aad88b7c0d8eb7
kyle <kyle.pxrk@gmail.com>
2023-07-13 12:23:14 +0900

    슬라이더블 구현

--- lib/teamMember.dart
+++ lib/teamMember.dart
@@ -1,6 +1,7 @@
 // team_member.dart
 
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:provider/provider.dart';
 import 'package:teampage/enroll_service.dart';
 import 'package:teampage/member_delete.dart';
@@ -37,69 +38,90 @@ class _TeamMemberState extends State<TeamMember> {
                   itemBuilder: (context, index) {
                     Enroll enroll = memberList[index];
 
-                    return Padding(
-                      padding: EdgeInsets.only(
-                        left: 15,
-                        top: 15,
-                        bottom: 3,
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    return Slidable(
+                      key: UniqueKey(),
+                      endActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        dismissible: DismissiblePane(
+                          onDismissed: () {
+                            enrollService.deleteMember(index);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("멤버가 삭제 되었습니다!")));
+                          },
+                        ),
                         children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.network(
-                              "https://cdn2.thecatapi.com/images/6bt.jpg",
-                              width: 150,
-                              height: 180,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          SizedBox(width: 13),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(height: 7),
-                                Text(
-                                  '이름 : ${enroll.name}',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'MBTI : ${enroll.mbti}',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  '장점 : ${enroll.advantage}',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  '협업스타일 : ${enroll.style}',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                SizedBox(height: 8),
-                                Text(
-                                  'URL : ${enroll.url}',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ],
-                            ),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return MemberDelete(index: index);
-                                },
-                              );
-                            },
+                          SlidableAction(
+                            onPressed: (context) {},
+                            backgroundColor: Colors.red,
+                            icon: Icons.delete,
+                            label: 'Delete',
                           ),
                         ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: 15,
+                          top: 15,
+                          bottom: 3,
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: Image.network(
+                                "https://cdn2.thecatapi.com/images/6bt.jpg",
+                                width: 150,
+                                height: 180,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            SizedBox(width: 13),
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 7),
+                                  Text(
+                                    '이름 : ${enroll.name}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'MBTI : ${enroll.mbti}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    '장점 : ${enroll.advantage}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    '협업스타일 : ${enroll.style}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'URL : ${enroll.url}',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return MemberDelete(index: index);
+                                  },
+                                );
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     );
                   },
